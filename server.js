ecc857a745a591860255cd1e51e829f26e19f52a
Kelvin Lau <kelvinkctl@gmail.com>
2023-08-03 20:24:02 -0400

    update

--- server.js
+++ server.js
@@ -10,33 +10,10 @@ const passportJWT = require("passport-jwt");
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-let ExtractJwt = passportJWT.ExtractJwt;
-let JwtStrategy = passportJWT.Strategy;
-
-let jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
-    secretOrKey: process.env.JWT_SECRET
-};
-
-let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-    console.log('payload received', jwt_payload);
-  
-    if (jwt_payload) {
-      next(null, {
-        _id: jwt_payload._id,
-        userName: jwt_payload.userName,
-      });
-    } else {
-      next(null, false);
-    }
-});
-
 
 app.use(express.json());
 app.use(cors());
 
-passport.use(strategy);
-app.use(passport.initialize());
 
 app.post("/api/user/register", (req, res) => {
     userService.registerUser(req.body)
